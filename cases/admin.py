e1cd12839052076531ccab05721c609f7b87c31c
Uktamjon <uktamforjob@gmail.com>
2021-10-01 19:53:39 +0500

    agent model

--- cases/admin.py
+++ cases/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from . import models
+
+
+User = get_user_model()
+
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "agent_type", "main_phone", "email", "status"]
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(models.Agent, AgentAdmin)
