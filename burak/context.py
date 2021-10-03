fce9ca7f5a881542c660a6338adce941843793ed
Uktamjon <uktamforjob@gmail.com>
2021-10-03 22:51:26 +0500

    bug fixes in context

--- burak/context.py
+++ burak/context.py
@@ -1,38 +1,41 @@
 from vehicles.models import *
 
 def vehicle(request):
-    path = request.path
-
-    path_splitted = path.split("/")
-
-    id = path_splitted[3]
-
     try:
-        id = int(id)
+        path = request.path
+
+        path_splitted = path.split("/")
+
+        id = path_splitted[3]
+
+        try:
+            id = int(id)
+        except:
+            id = None
+        
+        if not id:
+            return {"vehicle": None}
+
+        vehicle = None
+        
+        if path_splitted[2] == "hire":
+            vehicle = Hire.objects.get(id=id).vehicle
+
+        if path_splitted[2] == "service":
+            vehicle = Service.objects.get(id=id).vehicle
+
+        if path_splitted[2] == "supplier":
+            vehicle = Supplier.objects.get(id=id).vehicle
+
+        if path_splitted[2] == "finance":
+            vehicle = Finance.objects.get(id=id).vehicle
+        
+        if path_splitted[2] == "sale":
+            vehicle = Sale.objects.get(id=id).vehicle
+
+        if path_splitted[2] == "vehicle":
+            vehicle = Vehicle.objects.get(id=id)
+
+        return {"vehicle": vehicle}
     except:
-        id = None
-    
-    if not id:
-        return {"vehicle": None}
-
-    vehicle = None
-    
-    if path_splitted[2] == "hire":
-        vehicle = Hire.objects.get(id=id).vehicle
-
-    if path_splitted[2] == "service":
-        vehicle = Service.objects.get(id=id).vehicle
-
-    if path_splitted[2] == "supplier":
-        vehicle = Supplier.objects.get(id=id).vehicle
-
-    if path_splitted[2] == "finance":
-        vehicle = Finance.objects.get(id=id).vehicle
-    
-    if path_splitted[2] == "sale":
-        vehicle = Sale.objects.get(id=id).vehicle
-
-    if path_splitted[2] == "vehicle":
-        vehicle = Vehicle.objects.get(id=id)
-
-    return {"vehicle": vehicle}
+        return {}
